ab1e635df2fef467a0feea13c46be40a08644737
Jerry Cao <79422097+JerryFhuurr@users.noreply.github.com>
2024-06-28 15:29:20 +0200

    init commit

---
+++ src/main/java/com/bpr/bprbackend2/BprBackend2Application.java
@@ -0,0 +1,13 @@
+package com.bpr.bprbackend2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BprBackend2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BprBackend2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/bpr/bprbackend2/BprBackend2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bpr.bprbackend2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BprBackend2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
